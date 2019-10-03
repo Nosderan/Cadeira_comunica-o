13ce0cbf07f0d345ce2d400e30d18398f993f613
Nosderan <43793226+Nosderan@users.noreply.github.com>
2019-10-03 17:16:06 -0300

    Add files via upload

---
+++ tchaprimaboçal/adafruit/adafruit.ino
@@ -0,0 +1,266 @@
+#include <BearSSLHelpers.h>
+#include <CertStoreBearSSL.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiClientSecureAxTLS.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <WiFiServer.h>
+#include <WiFiServerSecure.h>
+#include <WiFiServerSecureAxTLS.h>
+#include <WiFiServerSecureBearSSL.h>
+#include <WiFiUdp.h>
+#include <Wire.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#define WIFI_SSID "saco"
+#define WIFI_PASS "somosgay"
+
+#define MQTT_SERV "io.adafruit.com"
+#define MQTT_PORT 1883
+#define MQTT_NAME "KelvenAraujo"
+#define MQTT_PASS "c7467d34b44842a787d030788b61b4f5"
+
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERV, MQTT_PORT, MQTT_NAME, MQTT_PASS);
+
+//Adafruit_MQTT_Subscribe onoff = Adafruit_MQTT_Subscribe(&mqtt, MQTT_NAME "/f/O");
+Adafruit_MQTT_Publish acx = Adafruit_MQTT_Publish(&mqtt, MQTT_NAME "/f/Acx");
+Adafruit_MQTT_Publish acy = Adafruit_MQTT_Publish(&mqtt, MQTT_NAME "/f/AcY");
+Adafruit_MQTT_Publish acz = Adafruit_MQTT_Publish(&mqtt, MQTT_NAME "/f/AcZ");
+Adafruit_MQTT_Publish girox = Adafruit_MQTT_Publish(&mqtt, MQTT_NAME "/f/giroX");
+Adafruit_MQTT_Publish giroy = Adafruit_MQTT_Publish(&mqtt, MQTT_NAME "/f/giroY");
+Adafruit_MQTT_Publish giroz = Adafruit_MQTT_Publish(&mqtt, MQTT_NAME "/f/giroZ");
+
+const int MPU_ADDR =      0x68; // definição do endereço do sensor MPU6050 (0x68)
+const int WHO_AM_I =      0x75; // registro de identificação do dispositivo
+const int PWR_MGMT_1 =    0x6B; // registro de configuração do gerenciamento de energia
+const int GYRO_CONFIG =   0x1B; // registro de configuração do giroscópio
+const int ACCEL_CONFIG =  0x1C; // registro de configuração do acelerômetro
+const int ACCEL_XOUT =    0x3B; // registro de leitura do eixo X do acelerômetro
+
+const int sda_pin = D5; // definição do pino I2C SDA
+const int scl_pin = D6; // definição do pino I2C SCL
+
+bool led_state = false;
+
+// variáveis para armazenar os dados "crus" do acelerômetro
+int16_t AcX, AcY, AcZ, GyX, GyY, GyZ;
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+
+  //Connect to WiFi
+  Serial.print("\n\nConnecting Wifi>");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(">");
+    delay(50);
+  }
+
+  Serial.println("OK!");
+}
+
+void loop()
+{
+  //Connect/Reconnect to MQTT
+  MQTT_connect();
+  acx.publish(AcX);
+}
+
+
+void MQTT_connect()
+{
+
+  //  // Stop if already connected
+  if (mqtt.connected() && mqtt.ping())
+  {
+    //    mqtt.disconnect();
+    return;
+  }
+
+  int8_t ret;
+
+  mqtt.disconnect();
+
+  Serial.print("Connecting to MQTT... ");
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) // connect will return 0 for connected
+  {
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+    retries--;
+    if (retries == 0)
+    {
+      ESP.reset();
+    }
+  }
+  Serial.println("MQTT Connected!");
+}
+
+void initI2C() 
+{
+  //Serial.println("---inside initI2C");
+  Wire.begin(sda_pin, scl_pin);
+}
+ 
+/*
+ * função que escreve um dado valor em um dado registro
+ */
+void writeRegMPU(int reg, int val)      //aceita um registro e um valor como parâmetro
+{
+  Wire.beginTransmission(MPU_ADDR);     // inicia comunicação com endereço do MPU6050
+  Wire.write(reg);                      // envia o registro com o qual se deseja trabalhar
+  Wire.write(val);                      // escreve o valor no registro
+  Wire.endTransmission(true);           // termina a transmissão
+}
+ 
+/*
+ * função que lê de um dado registro
+ */
+uint8_t readRegMPU(uint8_t reg)        // aceita um registro como parâmetro
+{
+  uint8_t data;
+  Wire.beginTransmission(MPU_ADDR);     // inicia comunicação com endereço do MPU6050
+  Wire.write(reg);                      // envia o registro com o qual se deseja trabalhar
+  Wire.endTransmission(false);          // termina transmissão mas continua com I2C aberto (envia STOP e START)
+  Wire.requestFrom(MPU_ADDR, 1);        // configura para receber 1 byte do registro escolhido acima
+  data = Wire.read();                   // lê o byte e guarda em 'data'
+  return data;                          //retorna 'data'
+}
+ 
+/*
+ * função que procura pelo sensor no endereço 0x68
+ */
+void findMPU(int mpu_addr)
+{
+  Wire.beginTransmission(MPU_ADDR);
+  int data = Wire.endTransmission(true);
+ 
+  if(data == 0)
+  {
+    Serial.print("Dispositivo encontrado no endereço: 0x");
+    Serial.println(MPU_ADDR, HEX);
+  }
+  else
+  {
+    Serial.println("Dispositivo não encontrado!");
+  }
+}
+
+/*
+ * função que verifica se o sensor responde e se está ativo
+ */
+void checkMPU(int mpu_addr)
+{
+  findMPU(MPU_ADDR);
+     
+  int data = readRegMPU(WHO_AM_I); // Register 117 – Who Am I - 0x75
+   
+  if(data == 104) 
+  {
+    Serial.println("MPU6050 Dispositivo respondeu OK! (104)");
+ 
+    data = readRegMPU(PWR_MGMT_1); // Register 107 – Power Management 1-0x6B
+ 
+    if(data == 64) Serial.println("MPU6050 em modo SLEEP! (64)");
+    else Serial.println("MPU6050 em modo ACTIVE!"); 
+  }
+  else Serial.println("Verifique dispositivo - MPU6050 NÃO disponível!");
+}
+ 
+/*
+ * função de inicialização do sensor
+ */
+void initMPU()
+{
+  setSleepOff();
+  setGyroScale();
+  setAccelScale();
+}
+ 
+/* 
+ *  função para configurar o sleep bit  
+ */
+void setSleepOff()
+{
+  writeRegMPU(PWR_MGMT_1, 0); // escreve 0 no registro de gerenciamento de energia(0x68), colocando o sensor em o modo ACTIVE
+}
+ 
+/* função para configurar as escalas do giroscópio
+   registro da escala do giroscópio: 0x1B[4:3]
+   0 é 250°/s
+ 
+    FS_SEL  Full Scale Range
+      0        ± 250 °/s      0b00000000
+      1        ± 500 °/s      0b00001000
+      2        ± 1000 °/s     0b00010000
+      3        ± 2000 °/s     0b00011000
+*/
+void setGyroScale()
+{
+  writeRegMPU(GYRO_CONFIG, 0);
+}
+ 
+/* função para configurar as escalas do acelerômetro
+   registro da escala do acelerômetro: 0x1C[4:3]
+   0 é 250°/s
+ 
+    AFS_SEL   Full Scale Range
+      0           ± 2g            0b00000000
+      1           ± 4g            0b00001000
+      2           ± 8g            0b00010000
+      3           ± 16g           0b00011000
+*/
+void setAccelScale()
+{
+  writeRegMPU(ACCEL_CONFIG, 0);
+}
+
+void readRawMPU()
+{  
+  Wire.beginTransmission(MPU_ADDR);       // inicia comunicação com endereço do MPU6050
+  Wire.write(ACCEL_XOUT);                       // envia o registro com o qual se deseja trabalhar, começando com registro 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);            // termina transmissão mas continua com I2C aberto (envia STOP e START)
+  Wire.requestFrom(MPU_ADDR, 14);         // configura para receber 14 bytes começando do registro escolhido acima (0x3B)
+ 
+  AcX = Wire.read() << 8;                 // lê primeiro o byte mais significativo
+  AcX |= Wire.read();                     // depois lê o bit menos significativo
+  AcY = Wire.read() << 8;
+  AcY |= Wire.read();
+  AcZ = Wire.read() << 8;
+  AcZ |= Wire.read();
+ 
+  GyX = Wire.read() << 8;
+  GyX |= Wire.read();
+  GyY = Wire.read() << 8;
+  GyY |= Wire.read();
+  GyZ = Wire.read() << 8;
+  GyZ |= Wire.read(); 
+ 
+  Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+ 
+  led_state = !led_state;
+  digitalWrite(LED_BUILTIN, led_state);         // pisca LED do NodeMCU a cada leitura do sensor
+  delay(50);                                        
+}
